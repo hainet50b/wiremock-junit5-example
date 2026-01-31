c3d07a9c497f17e77f1af1541713eae5fa11e09c
hainet50b <20400616+hainet50b@users.noreply.github.com>
2026-01-31 23:14:55 +0900

    Add examples for editing stubs via the WireMock admin HTTP client

--- src/test/java/UserClientTestUsingTestContainers.java
+++ src/test/java/UserClientTestUsingTestContainers.java
@@ -1,13 +1,16 @@
+import com.github.tomakehurst.wiremock.client.HttpAdminClient;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Map;
+import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Testcontainers
 class UserClientTestUsingTestContainers {
@@ -27,6 +30,44 @@ class UserClientTestUsingTestContainers {
                 () -> assertEquals(2, users.size()),
                 () -> assertEquals(new User(1, "hainet50b"), users.get(1)),
                 () -> assertEquals(new User(2, "programacho.com"), users.get(2))
+        );
+    }
+
+    @Test
+    void findAllTest_clientError() {
+        HttpAdminClient client = new HttpAdminClient(container.getHost(), container.getMappedPort(8080));
+
+        StubMapping stubMapping = client.getStubMapping(UUID.fromString("6f33f78c-da9d-4599-8621-3887e02ec1df")).getItem();
+        stubMapping.setResponse(aResponse()
+                .withStatus(400)
+                .build()
+        );
+        client.editStubMapping(stubMapping);
+
+        UserClient sut = new UserClientImpl(container.getBaseUrl());
+
+        assertThrows(
+                HttpClientErrorException.class,
+                sut::findAll
+        );
+    }
+
+    @Test
+    void findAllTest_serverError() {
+        HttpAdminClient client = new HttpAdminClient(container.getHost(), container.getMappedPort(8080));
+
+        StubMapping stubMapping = client.getStubMapping(UUID.fromString("6f33f78c-da9d-4599-8621-3887e02ec1df")).getItem();
+        stubMapping.setResponse(aResponse()
+                .withStatus(500)
+                .build()
+        );
+        client.editStubMapping(stubMapping);
+
+        UserClient sut = new UserClientImpl(container.getBaseUrl());
+
+        assertThrows(
+                HttpServerErrorException.class,
+                sut::findAll
         );
     }
 }
